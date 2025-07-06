ea896ef1e433f80fa0364c8f45e223a5841e5093
NoahOnFyre <noahonfyre@gmail.com>
2025-07-06 16:21:48 +0200

    feat: add utility class

---
+++ src/main/kotlin/com/nyronium/stardust/util/StardustUtils.kt
@@ -0,0 +1,15 @@
+package com.nyronium.stardust.util
+
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.item.enchantment.Enchantment
+import net.minecraft.world.item.enchantment.EnchantmentHelper
+
+object StardustUtils {
+    fun hasEnchantment(enchantment: Enchantment, entity: LivingEntity): Boolean {
+        return EnchantmentHelper.getEnchantmentLevel(enchantment, entity) > 0
+    }
+
+    fun getLevel(enchantment: Enchantment, entity: LivingEntity): Int {
+        return EnchantmentHelper.getEnchantmentLevel(enchantment, entity)
+    }
+}
