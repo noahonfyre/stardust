85ae6a29deb01f0360068d490a48404f70fba1bb
NoahOnFyre <noahonfyre@gmail.com>
2025-07-15 20:14:35 +0200

    fix: remove debugging messages in PlayerMixin

--- src/main/java/mixin/PlayerMixin.java
+++ src/main/java/mixin/PlayerMixin.java
@@ -2,8 +2,6 @@ package mixin;
 
 import com.nyronium.stardust.Stardust;
 import com.nyronium.stardust.misc.StardustUtils;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,9 +16,7 @@ public abstract class PlayerMixin {
 
     @Redirect(method = "checkMovementStatistics", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;causeFoodExhaustion(F)V"))
     private void checkMovementStatistics(Player instance, float pExhaustion) {
-        instance.sendSystemMessage(Component.literal("invoked"));
         if(!StardustUtils.INSTANCE.hasEnchantment(Stardust.INSTANCE.getENDURANCE().get(), instance.getItemBySlot(EquipmentSlot.LEGS))) {
-            instance.swing(InteractionHand.MAIN_HAND);
             causeFoodExhaustion(pExhaustion);
         } else {
             causeFoodExhaustion(pExhaustion/(1+StardustUtils.INSTANCE.getLevel(Stardust.INSTANCE.getENDURANCE().get(), instance.getItemBySlot(EquipmentSlot.LEGS))));
