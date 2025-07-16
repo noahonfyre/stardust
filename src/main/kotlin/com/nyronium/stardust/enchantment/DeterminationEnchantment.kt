84e1c6f6d7c30b87096c7c5b0ba83a7527d2bcbb
NoahOnFyre <noahonfyre@gmail.com>
2025-07-16 16:37:31 +0200

    fix(balancing): add incompatibility between determination and swift sneak

--- src/main/kotlin/com/nyronium/stardust/enchantment/DeterminationEnchantment.kt
+++ src/main/kotlin/com/nyronium/stardust/enchantment/DeterminationEnchantment.kt
@@ -2,8 +2,14 @@ package com.nyronium.stardust.enchantment
 
 import com.nyronium.stardust.enchantment.infrastructure.StardustEnchantment
 import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.item.enchantment.Enchantment
 import net.minecraft.world.item.enchantment.EnchantmentCategory
+import net.minecraft.world.item.enchantment.Enchantments
 
 class DeterminationEnchantment : StardustEnchantment(Rarity.VERY_RARE, EnchantmentCategory.ARMOR_LEGS, arrayOf(EquipmentSlot.LEGS)) {
     override fun getMaxLevel() = 4
+
+    override fun checkCompatibility(pOther: Enchantment): Boolean {
+        return super.checkCompatibility(pOther) && pOther != Enchantments.SWIFT_SNEAK
+    }
 }
