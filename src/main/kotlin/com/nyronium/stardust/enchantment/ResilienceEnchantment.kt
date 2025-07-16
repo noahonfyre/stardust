fad5ccffb06437f7fac7f7a28396d75e3963ba7a
NoahOnFyre <noahonfyre@gmail.com>
2025-07-16 16:16:53 +0200

    fix(balancing): adjust level multiplier for resilience

--- src/main/kotlin/com/nyronium/stardust/enchantment/ResilienceEnchantment.kt
+++ src/main/kotlin/com/nyronium/stardust/enchantment/ResilienceEnchantment.kt
@@ -21,25 +21,25 @@ class ResilienceEnchantment : StardustEnchantment(
             EquipmentSlot.HEAD,
             Attributes.ARMOR_TOUGHNESS,
             UUID.fromString("e9641845-df27-442a-8e28-a37faa659b92"),
-        ) { level -> 0.5*level }
+        ) { level -> 0.25*level }
 
         registerAttributeModifier(
             EquipmentSlot.CHEST,
             Attributes.ARMOR_TOUGHNESS,
             UUID.fromString("7683e4e5-31f5-406b-bbf2-6065e61519c9"),
-        ) { level -> 0.5*level }
+        ) { level -> 0.25*level }
 
         registerAttributeModifier(
             EquipmentSlot.LEGS,
             Attributes.ARMOR_TOUGHNESS,
             UUID.fromString("d7432655-4e69-4809-b1ef-ee3503c556c9"),
-        ) { level -> 0.5*level }
+        ) { level -> 0.25*level }
 
         registerAttributeModifier(
             EquipmentSlot.FEET,
             Attributes.ARMOR_TOUGHNESS,
             UUID.fromString("f18d0931-0d08-4546-a6fb-e9bb34615113"),
-        ) { level -> 0.5*level }
+        ) { level -> 0.25*level }
     }
 
     override fun getMaxLevel() = 4
