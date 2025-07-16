4c04583041fe73c404685c85345279fab41d5c78
NoahOnFyre <noahonfyre@gmail.com>
2025-07-16 16:36:17 +0200

    fix(balancing): add incompatibilities to similar enchantments

--- src/main/kotlin/com/nyronium/stardust/enchantment/DecayEnchantment.kt
+++ src/main/kotlin/com/nyronium/stardust/enchantment/DecayEnchantment.kt
@@ -1,5 +1,6 @@
 package com.nyronium.stardust.enchantment
 
+import com.nyronium.stardust.Stardust
 import com.nyronium.stardust.enchantment.infrastructure.StardustEnchantment
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.effect.MobEffects
@@ -7,7 +8,9 @@ import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.enchantment.Enchantment
 import net.minecraft.world.item.enchantment.EnchantmentCategory
+import net.minecraft.world.item.enchantment.Enchantments
 
 class DecayEnchantment : StardustEnchantment(Rarity.RARE, EnchantmentCategory.WEAPON, arrayOf(EquipmentSlot.MAINHAND)) {
     override fun getMaxLevel() = 2
@@ -20,5 +23,9 @@ class DecayEnchantment : StardustEnchantment(Rarity.RARE, EnchantmentCategory.WE
         if(random <= 2.5*pLevel) {
             pTarget.addEffect(MobEffectInstance(MobEffects.WITHER, 60, pLevel-1))
         }
+    }
+
+    override fun checkCompatibility(pOther: Enchantment): Boolean {
+        return super.checkCompatibility(pOther) && pOther != Enchantments.FIRE_ASPECT && pOther != Stardust.FROSTBITE && pOther != Stardust.ELECTROCUTION
     }
 }
