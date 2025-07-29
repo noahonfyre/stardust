fdc9d1566dcaf84c2e3e7892a717896106ba8c19
NoahOnFyre <noahonfyre@gmail.com>
2025-07-30 00:53:30 +0200

    fix: adjust charisma to trigger a special price instead of resetting trade offers

---
+++ src/main/java/mixin/VillagerMixin.java
@@ -0,0 +1,28 @@
+package mixin;
+
+import com.nyronium.stardust.core.StardustRegistry;
+import com.nyronium.stardust.core.StardustUtils;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.trading.MerchantOffer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Villager.class)
+public class VillagerMixin {
+    @Inject(method = "updateSpecialPrices", at = @At("HEAD"))
+    private void updateSpecialPrices(Player pPlayer, CallbackInfo ci) {
+        if(!StardustUtils.INSTANCE.hasEnchantment(StardustRegistry.INSTANCE.getCHARISMA().get(), pPlayer.getItemBySlot(EquipmentSlot.HEAD))) return;
+        int level = StardustUtils.INSTANCE.getLevel(StardustRegistry.INSTANCE.getCHARISMA().get(), pPlayer.getItemBySlot(EquipmentSlot.HEAD));
+
+        Villager self = (Villager) (Object) this;
+
+        for(MerchantOffer offer : self.getOffers()) {
+            int value = Math.toIntExact(Math.round(Math.floor(((double) offer.getBaseCostA().getCount()) / 8 * level)));
+            offer.setSpecialPriceDiff(-value);
+        }
+    }
+}
