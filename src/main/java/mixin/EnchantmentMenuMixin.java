428bff8f1cfd3732113d524f91aa9836d6ef91a3
NoahOnFyre <noahonfyre@gmail.com>
2025-07-31 00:05:13 +0200

    refactor: adjust cost manager references

--- src/main/java/mixin/EnchantmentMenuMixin.java
+++ src/main/java/mixin/EnchantmentMenuMixin.java
@@ -1,6 +1,6 @@
 package mixin;
 
-import com.nyronium.stardust.core.ExperienceCostManager;
+import com.nyronium.stardust.content.experience.CostManager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.EnchantmentMenu;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class EnchantmentMenuMixin {
     @Inject(method = "clickMenuButton", at = @At("HEAD"))
     private void clickMenuButton(Player pPlayer, int pId, CallbackInfoReturnable<Boolean> cir) {
-        ExperienceCostManager.INSTANCE.getSelectedLocal().set(pId);
+        CostManager.INSTANCE.getSelectedLocal().set(pId);
     }
 }
--- src/main/java/mixin/PlayerMixin.java
+++ src/main/java/mixin/PlayerMixin.java
@@ -1,6 +1,6 @@
 package mixin;
 
-import com.nyronium.stardust.core.ExperienceCostManager;
+import com.nyronium.stardust.content.experience.CostManager;
 import com.nyronium.stardust.core.StardustRegistry;
 import com.nyronium.stardust.core.StardustUtils;
 import net.minecraft.core.NonNullList;
@@ -46,6 +46,6 @@ public abstract class PlayerMixin {
 
     @Redirect(method = "onEnchantmentPerformed", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;giveExperienceLevels(I)V"))
     private void onEnchantmentPerformed(Player instance, int levels) {
-        ExperienceCostManager.INSTANCE.handleEnchantXp(instance, levels);
+        CostManager.INSTANCE.handleEnchantXp(instance, levels);
     }
 }
