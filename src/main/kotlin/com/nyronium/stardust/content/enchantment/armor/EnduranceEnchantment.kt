94e5630b802e73da41585ecd6d61e02bdfaf0f20
NoahOnFyre <noahonfyre@gmail.com>
2025-08-01 11:29:16 +0200

    feat(endurance): add to village cartographer loot table

--- src/main/kotlin/com/nyronium/stardust/content/enchantment/armor/EnduranceEnchantment.kt
+++ src/main/kotlin/com/nyronium/stardust/content/enchantment/armor/EnduranceEnchantment.kt
@@ -8,7 +8,7 @@ import net.minecraft.world.item.enchantment.EnchantmentCategory
 
 class EnduranceEnchantment : StardustEnchantment(EnchantmentConfiguration()
     .maxLevel(4)
-    .obtaining(ObtainingConfiguration(Rarity.COMMON).default())
+    .obtaining(ObtainingConfiguration(Rarity.COMMON).default().loot("chests/village/village_cartographer", 0.75f))
     .category(EnchantmentCategory.ARMOR_LEGS)
     .applicableSlots(EquipmentSlot.LEGS)
 )
