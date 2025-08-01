a8cfc14d6ba448fb455f66126a544a648217062b
NoahOnFyre <noahonfyre@gmail.com>
2025-08-01 03:05:06 +0200

    feat: add nullification to desert pyramids

--- src/main/kotlin/com/nyronium/stardust/content/enchantment/armor/NullificationEnchantment.kt
+++ src/main/kotlin/com/nyronium/stardust/content/enchantment/armor/NullificationEnchantment.kt
@@ -1,9 +1,9 @@
 package com.nyronium.stardust.content.enchantment.armor
 
+import com.nyronium.stardust.core.StardustUtils
 import com.nyronium.stardust.core.infrastructure.EnchantmentConfiguration
 import com.nyronium.stardust.core.infrastructure.ObtainingConfiguration
 import com.nyronium.stardust.core.infrastructure.StardustEnchantment
-import com.nyronium.stardust.core.StardustUtils
 import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.enchantment.EnchantmentCategory
@@ -12,7 +12,7 @@ import thedarkcolour.kotlinforforge.forge.FORGE_BUS
 
 class NullificationEnchantment : StardustEnchantment(EnchantmentConfiguration()
     .maxLevel(5)
-    .obtaining(ObtainingConfiguration(Rarity.RARE).default().notTradable())
+    .obtaining(ObtainingConfiguration(Rarity.RARE).default().notTradable().loot("chests/desert_pyramid", 0.25f))
     .category(EnchantmentCategory.ARMOR_CHEST)
     .applicableSlots(EquipmentSlot.CHEST)
 ) {
