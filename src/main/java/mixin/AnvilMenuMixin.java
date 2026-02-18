1aa57af59070a5de4465396deb5fb956b7346935
NoahOnFyre <noahonfyre@gmail.com>
2026-02-18 15:11:32 +0100

    feat: add customizability to anvil mechanics

--- src/main/java/mixin/AnvilMenuMixin.java
+++ src/main/java/mixin/AnvilMenuMixin.java
@@ -1,15 +1,17 @@
 package mixin;
 
 import com.nyronium.stardust.content.experience.ExperienceManager;
+import com.nyronium.stardust.core.config.ConfigHandler;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AnvilMenu;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = AnvilMenu.class, priority = 1)
+@Mixin(value = AnvilMenu.class, priority = 2000)
 public class AnvilMenuMixin {
     @ModifyConstant(method = "createResult", constant = @Constant(intValue = 40), require = 0)
     private int removeLimit(int original) {
@@ -18,7 +20,21 @@ public class AnvilMenuMixin {
 
     @ModifyConstant(method = "createResult", constant = @Constant(intValue = 39), require = 0)
     private int maxLimit(int original) {
-        return Integer.MAX_VALUE - 1;
+        int anvilCap = ConfigHandler.INSTANCE.getAnvilCap().get();
+        if(anvilCap == 0) {
+            return Integer.MAX_VALUE - 1;
+        } else {
+            return anvilCap;
+        }
+    }
+
+    @Redirect(method = "createResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getBaseRepairCost()I"))
+    private int createResult(ItemStack instance) {
+        if(ConfigHandler.INSTANCE.getDisablePriorWorkPenalty().get()) {
+            return 0;
+        } else {
+            return instance.getBaseRepairCost();
+        }
     }
 
     @Redirect(method = "onTake", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;giveExperienceLevels(I)V"))
--- src/main/kotlin/com/nyronium/stardust/core/config/ConfigHandler.kt
+++ src/main/kotlin/com/nyronium/stardust/core/config/ConfigHandler.kt
@@ -8,13 +8,19 @@ object ConfigHandler {
     private val BUILDER = ForgeConfigSpec.Builder()
 
     var disabledEnchantments: ForgeConfigSpec.ConfigValue<MutableList<out String>> = BUILDER
-        .comment(
-            " A list of disabled enchantments by their enchantment ID (e.g. stardust:tenacity)"
-        )
+        .comment(" A list of disabled enchantments by their enchantment ID. (e.g. stardust:tenacity)")
         .defineList(
             "disabled_enchantments",
             mutableListOf<String>()
-        ) { obj -> obj is String }
+        ) { obj -> obj is String && obj.startsWith("stardust:")}
+
+    var anvilCap: ForgeConfigSpec.IntValue = BUILDER
+        .comment(" The maximum amount of levels that the player will lose during anvil transactions. (default: 60, set to 0 to remove limit)")
+        .defineInRange("anvil_cap", 90, 0, Int.MAX_VALUE-1)
+
+    var disablePriorWorkPenalty: ForgeConfigSpec.BooleanValue = BUILDER
+        .comment(" Toggles the prior work penalty mechanic, which makes an item more expensive to combine in an anvil.")
+        .define("disable_prior_work_penalty", false)
 
 
     val SPEC: ForgeConfigSpec = BUILDER.build()
